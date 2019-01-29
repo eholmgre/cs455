ca78f69acca0f358e9a2ecb0dc192480979a4e73
erik <erik@erikholmgren.com>
2019-01-28 19:35:58 -0700

    Updates to node classes

--- overlay/src/main/java/cs455/overlay/node/MessagingNode.java
+++ overlay/src/main/java/cs455/overlay/node/MessagingNode.java
@@ -1,4 +1,14 @@
 package cs455.overlay.node;
 
+import cs455.overlay.wireformats.Event;
+
 public class MessagingNode implements Node{
+    @Override
+    public void onEvent(Event event) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- overlay/src/main/java/cs455/overlay/node/Registry.java
+++ overlay/src/main/java/cs455/overlay/node/Registry.java
@@ -1,4 +1,14 @@
 package cs455.overlay.node;
 
+import cs455.overlay.wireformats.Event;
+
 public class Registry implements Node{
+    @Override
+    public void onEvent(Event event) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
