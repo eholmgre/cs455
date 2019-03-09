e76e7e84a94ff68234e93f871f7284eb189cffdd
erik <erik@erikholmgren.com>
2019-03-09 13:43:35 -0700

    arg parsin

--- scaling/src/main/java/cs455/scaling/client/Client.java
+++ scaling/src/main/java/cs455/scaling/client/Client.java
@@ -1,5 +1,7 @@
 package cs455.scaling.client;
 
+import java.net.InetSocketAddress;
+
 public class Client {
 
     String []pargs;
@@ -8,8 +10,48 @@ public class Client {
         pargs = args;
     }
 
-    public void start() {
+    public void printUsage() {
+        System.out.println("Scaling Client Usage:\n\tclient <server host> <server port> <message rate>");
+    }
 
+    public void start() {
+        // java cs455.scaling.client.Client server-host server-port message-rate
+
+        if (pargs.length != 3) {
+            printUsage();
+            return;
+        }
+
+        InetSocketAddress serverAddr = null;
+        int messageRate = -1;
+
+        boolean parseSuccess = true;
+        try {
+            int serverPort = -1;
+            serverPort = Integer.parseInt(pargs[1]);
+            messageRate = Integer.parseInt(pargs[2]);
+
+            serverAddr = new InetSocketAddress(pargs[0], serverPort);
+
+        } catch (NumberFormatException e) {
+            parseSuccess = false;
+        } finally {
+            // lol i dont even know what this really does but it seems to work
+            if (serverAddr == null || serverAddr.isUnresolved()) {
+                System.out.println("Invalid server host/port " + serverAddr);
+                parseSuccess = false;
+            }
+
+            if (messageRate < 1) {
+                System.out.println("Invalid message rate \"" + pargs[2] + "\"");
+                parseSuccess = false;
+            }
+
+            if (! parseSuccess) {
+                printUsage();
+                return;
+            }
+        }
     }
 
     public static void main(String []args) {
--- scaling/src/main/java/cs455/scaling/server/Server.java
+++ scaling/src/main/java/cs455/scaling/server/Server.java
@@ -11,7 +11,62 @@ public class Server {
         pargs = args;
     }
 
+    public void printUsage() {
+        System.out.println("Scaling Server Usage:\n\tserver <portnum> <thread pool size> <batch size> <batch time>");
+    }
+
+
     public void start() {
+
+        int portNum = -1;
+        int numThreads = -1;
+        int batchSize = -1;
+        int batchTime = -1;
+
+        if (pargs.length != 4) {
+            printUsage();
+            return;
+        }
+
+        boolean parseSuccess = true;
+        try {
+            portNum = Integer.parseInt(pargs[0]);
+            numThreads = Integer.parseInt(pargs[1]);
+            batchSize = Integer.parseInt(pargs[2]);
+            batchTime = Integer.parseInt(pargs[3]);
+        } catch (NumberFormatException e) {
+            parseSuccess = false;
+        } finally {
+            if (portNum < 1025 || portNum > Math.pow(2, 16) ) {
+                System.out.println("Invalid port number \"" + pargs[0] + "\"");
+                parseSuccess = false;
+            }
+
+            if (numThreads < 1) {
+                System.out.println("Invalid thread count \"" + pargs[1] + "\"");
+                parseSuccess = false;
+            }
+
+            if (batchSize < 1) {
+                System.out.println("Invalid batch size \"" + pargs[2] + "\"");
+                parseSuccess = false;
+            }
+
+            if (batchTime < 1) {
+                // TODO: what about non integer times, is that allowed?
+                System.out.println("Invalid batch time \"" + pargs[3] + "\"");
+                parseSuccess = false;
+            }
+
+            if (! parseSuccess) {
+                printUsage();
+                return;
+            }
+        }
+
+        // NIO setup?
+
+        // threadpool setup / start?
 
     }
 
